5006744cb319a0b8b24f9e3f90cfe16992ef9a1a
ellysamuz-droid <ellysamuz@student.uns.ac.id>
2026-04-23 18:44:14 +0700

    update

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,12 +1,30 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "reminderimunisasi";
+// Data dari TiDB Cloud
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$port = 4000;
+$user = '4E4R7ePMi5xj2AM.root';
+$pass = 'YJWuAMvg2BFEYIzj';
+$db   = 'imun';
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+// Inisialisasi mysqli
+$koneksi = mysqli_init();
 
-if (!$koneksi) {
-    die("Koneksi dengan database gagal: " . mysqli_connect_error());
+// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
 }
 ?>
